69dfeb6b647031970de42df21222487971941f83
ecankaya <emrecankaya00@hotmail.com>
2023-05-29 20:16:25 +0200

    second

--- mlflow_ui.py
+++ mlflow_ui.py
@@ -1,3 +1,2 @@
-from zenml.environment import Environment
 from zenml.integrations.mlflow.mlflow_utils import get_tracking_uri
 print({get_tracking_uri()})
--- pipelines.py
+++ pipelines.py
@@ -1,8 +1,5 @@
 from zenml import pipeline
-from steps import training_data_loader,tf_trainer,evaluator
-from zenml.integrations.mlflow.steps import mlflow_model_deployer_step
-model_deployer=mlflow_model_deployer_step
-
+from steps import training_data_loader, tf_trainer, evaluator
 
 
 @pipeline(enable_cache=False)
@@ -12,5 +9,6 @@ def training_pipeline(
     x_train, x_test, y_train, y_test = training_data_loader()
     model = tf_trainer(x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test)
     test_acc = evaluator(x_test=x_test, y_test=y_test, model=model)
+
 
 training_pipeline()
